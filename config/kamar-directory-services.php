2c3dbea16a89ad1d843ef2370d9c305526e92f6a
Sean Anderson <wing5wong@hotmail.com>
2023-03-08 08:50:06 +1300

    whitespace

--- config/kamar-directory-services.php
+++ config/kamar-directory-services.php
@@ -11,6 +11,7 @@ return [
     'serviceVersion' => 1.0,
 
     'authSuffix' => env('KAMAR_DS_AUTH_SUFFIX'),
+    
     'infoUrl' => 'https://www.myschool.co.nz/more-info',
     'privacyStatement' => 'Change this to a valid privacy statement | All your data belongs to us and will be kept in a top secret vault.',
 
