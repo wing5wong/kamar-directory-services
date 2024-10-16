7d49457eafcd4f9a4a61b98c2510135e74373ae4
Sean Anderson <wing5wong@hotmail.com>
2024-10-16 22:47:00 +1300

    fix groupType and message transposed

--- src/Emergency/LogEmergencyService.php
+++ src/Emergency/LogEmergencyService.php
@@ -9,8 +9,8 @@ class LogEmergencyService implements EmergencyServiceInterface
     public function notify(EmergencyData $data)
     {
         Log::info([
-            "message" => $data->groupType,
-            "groupType" => $data->message,
+            "message" => $data->message,
+            "groupType" => $data->groupType,
             "id" => $data->id,
             "isEmergency" => $data->isEmergency,
             "procedure" => $data->procedure,
