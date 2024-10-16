c59f6e16318de84f0cbb01d24c2db0647abfd9f4
Sean Anderson <wing5wong@hotmail.com>
2024-10-16 22:43:50 +1300

    add log keys and use Log facade not helper

--- src/Emergency/LogEmergencyService.php
+++ src/Emergency/LogEmergencyService.php
@@ -2,18 +2,20 @@
 
 namespace Wing5wong\KamarDirectoryServices\Emergency;
 
+use Illuminate\Support\Facades\Log;
+
 class LogEmergencyService implements EmergencyServiceInterface
 {
     public function notify(EmergencyData $data)
     {
-        info([
-            $data->message,
-            $data->groupType,
-            $data->id,
-            $data->isEmergency,
-            $data->procedure,
-            $data->status,
-            $data->unixTime,
+        Log::info([
+            "message" => $data->groupType,
+            "groupType" => $data->message,
+            "id" => $data->id,
+            "isEmergency" => $data->isEmergency,
+            "procedure" => $data->procedure,
+            "status" => $data->status,
+            "unixTime" => $data->unixTime,
         ]);
     }
 }
