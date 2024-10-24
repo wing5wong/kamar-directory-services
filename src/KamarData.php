981ce1f96f0086f9a34d116599321de0bf5ac3ee
Sean Anderson <wing5wong@hotmail.com>
2024-10-24 13:13:27 +1300

    updated request-xml, dont need empty check here now

--- src/KamarData.php
+++ src/KamarData.php
@@ -10,16 +10,16 @@ class KamarData
     const SYNC_TYPE_CHECK = 'check';
     const SYNC_TYPE_PART = 'part';
     const SYNC_TYPE_FULL = 'full';
-    const SYNC_TYPE_ASSESSMENTS ='assessments';
-    const SYNC_TYPE_ATTENDANCE ='attendance';
-    const SYNC_TYPE_BOOKINGS ='bookings';
-    const SYNC_TYPE_CALENDAR ='calendar';
-    const SYNC_TYPE_NOTICES ='notices';
-    const SYNC_TYPE_PASTORAL ='pastoral';
-    const SYNC_TYPE_PHOTOS ='photos';
-    const SYNC_TYPE_STAFFPHOTOS ='staffphotos';
-    const SYNC_TYPE_STUDENTTIMETABLES ='studenttimetables';
-    const SYNC_TYPE_STAFFTIMETABLES ='stafftimetables';
+    const SYNC_TYPE_ASSESSMENTS = 'assessments';
+    const SYNC_TYPE_ATTENDANCE = 'attendance';
+    const SYNC_TYPE_BOOKINGS = 'bookings';
+    const SYNC_TYPE_CALENDAR = 'calendar';
+    const SYNC_TYPE_NOTICES = 'notices';
+    const SYNC_TYPE_PASTORAL = 'pastoral';
+    const SYNC_TYPE_PHOTOS = 'photos';
+    const SYNC_TYPE_STAFFPHOTOS = 'staffphotos';
+    const SYNC_TYPE_STUDENTTIMETABLES = 'studenttimetables';
+    const SYNC_TYPE_STAFFTIMETABLES = 'stafftimetables';
 
     public $data;
     public $syncType;
@@ -28,20 +28,20 @@ class KamarData
     public function isMissing()
     {
         return $this->format == 'json'
-        ? empty(data_get($this->data, 'SMSDirectoryData'))
-        : $this->data->isEmpty();
+            ? empty(data_get($this->data, 'SMSDirectoryData'))
+            : $this->data->isEmpty();
     }
 
     public function getSyncType()
     {
         return $this->format == 'json'
-        ? data_get($this->data,  'SMSDirectoryData.sync')
-        : data_get($this->data,  '@attributes.sync');
+            ? data_get($this->data,  'SMSDirectoryData.sync')
+            : data_get($this->data,  '@attributes.sync');
     }
 
     public function isSyncType($syncType)
     {
-        return $this->getSyncType() === $syncType; 
+        return $this->getSyncType() === $syncType;
     }
 
     public function isSyncCheck()
@@ -81,16 +81,12 @@ class KamarData
 
         if (request()->isJson()) {
             $kamarData->setData(collect(request()->input()), 'json');
-        }
-        elseif (request()->isXml()) {
-            $data = request()->getContent() > ''
-            ? collect( request()->xml(true))
-            : collect([]);
-            $kamarData->setData($data, 'xml');
-        }
-        else {
+        } elseif (request()->isXml()) {
+            $kamarData->setData(collect(request()->xml()), 'xml');
+        } else {
             throw new Exception("Invalid content");
         }
+
         return $kamarData;
     }
 
@@ -100,7 +96,7 @@ class KamarData
         if ($useBasePath) {
             $kamarData->setData(collect(json_decode(Storage::disk('local')->get('data/' . $filename), true)), 'json');
         } else {
-            $kamarData->setData(collect(json_decode(file_get_contents($filename), true)) , 'json');
+            $kamarData->setData(collect(json_decode(file_get_contents($filename), true)), 'json');
         }
         return $kamarData;
     }
@@ -118,15 +114,15 @@ class KamarData
     public function getStaffTimetables()
     {
         return $this->syncType == self::SYNC_TYPE_STAFFTIMETABLES
-        ? collect(data_get($this->data, 'SMSDirectoryData.timetables.data'))
-        : collect([]);
+            ? collect(data_get($this->data, 'SMSDirectoryData.timetables.data'))
+            : collect([]);
     }
 
     public function getStudentTimetables()
     {
         return $this->syncType == self::SYNC_TYPE_STUDENTTIMETABLES
-        ? collect(data_get($this->data, 'SMSDirectoryData.timetables.data'))
-        : collect([]);
+            ? collect(data_get($this->data, 'SMSDirectoryData.timetables.data'))
+            : collect([]);
     }
 
     public function getAttendance()
