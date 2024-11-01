57ed9df1d2bd7861e69d5e03ee3649c9850d25a4
Sean Anderson <wing5wong@hotmail.com>
2024-11-01 18:23:24 +1300

    reorganise tests

--- tests/Feature/DirectoryServiceRequestTest.php
+++ tests/Feature/DirectoryServiceRequestTest.php
@@ -9,32 +9,51 @@ use Wing5wong\KamarDirectoryServices\Tests\TestCase;
 
 class DirectoryServiceRequestTest extends TestCase
 {
-    public function test_unauthorised_requests_throw_exception()
+    public function test_wrong_accept_header_throws_exception()
     {
         $this->withoutExceptionHandling();
         $this->expectException(Exception::class);
-        $this->post('/kamar', [], [
+        $this->post(route('kamar'), [], [
             'Accept' => 'not-x-m-l-or-json'
         ]);
     }
 
     public function test_unauthorised_json_requests_return_failed_authentication_response()
     {
-        $response = $this->postJson('/kamar');
+        $response = $this->postJson(route('kamar'));
         $response->assertJson((new FailedAuthentication())->toArray(), true);
+    }
+
+    public function test_json_requests_with_invalid_credentials()
+    {
+        $response = $this->withHeaders([
+            'HTTP_AUTHORIZATION' => $this->invalidCredentials(),
+        ])->postJson(route('kamar'));
+
+        $response->assertJson((new FailedAuthentication())->toArray());
     }
 
     public function test_unauthorised_xml_requests_return_failed_authentication_response()
     {
-        $response = $this->post('/kamar', [], [
+        $response = $this->post(route('kamar'), [], [
             'content-type' => 'application/xml'
         ]);
         $this->assertSame((string)(new XMLFailedAuthentication()), $response->getContent());
     }
 
+    public function test_xml_requests_with_invalid_credentials()
+    {
+        $response = $this->withHeaders([
+            'HTTP_AUTHORIZATION' => $this->invalidCredentials(),
+            'content-type' => 'application/xml'
+        ])->post(route('kamar'));
+
+        $this->assertSame((string)(new XMLFailedAuthentication()), $response->getContent());
+    }
+
     public function test_authorised_json_requests_allowed()
     {
-        $response = $this->postJson('/kamar', [], [
+        $response = $this->postJson(route('kamar'), [], [
             'HTTP_AUTHORIZATION' => $this->validCredentials(),
         ]);
 
@@ -43,7 +62,7 @@ class DirectoryServiceRequestTest extends TestCase
 
     public function test_authorised_xml_requests_allowed()
     {
-        $response = $this->post('/kamar', [], [
+        $response = $this->post(route('kamar'), [], [
             'HTTP_AUTHORIZATION' => $this->validCredentials(),
             'content-type' => 'application/xml',
         ]);
@@ -54,5 +73,10 @@ class DirectoryServiceRequestTest extends TestCase
     private function validCredentials()
     {
         return "Basic " . base64_encode(config('kamar-directory-services.username') . ':' . config('kamar-directory-services.password'));
+    }
+
+    private function invalidCredentials()
+    {
+        return "Basic " . base64_encode('username' . ':' . 'password');
     }
 }
---
+++ tests/Feature/HandleKamarPostTest.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Wing5wong\KamarDirectoryServices\Tests\Feature;
+
+use Illuminate\Support\Facades\Storage;
+use Wing5wong\KamarDirectoryServices\KamarData;
+use Wing5wong\KamarDirectoryServices\Responses\Standard\Success;
+use Wing5wong\KamarDirectoryServices\Responses\Check\Success as CheckSuccess;
+use Wing5wong\KamarDirectoryServices\Responses\Standard\MissingData;
+use Wing5wong\KamarDirectoryServices\Responses\Standard\XMLMissingData;
+use Wing5wong\KamarDirectoryServices\Tests\TestCase;
+
+class HandleKamarPostTest extends TestCase
+{
+
+    public function test_json_request_with_missing_data()
+    {
+        $response = $this->validJsonCredentialRequest();
+        $response->assertJson((new MissingData())->toArray(), true);
+    }
+
+    public function test_xml_request_with_missing_data()
+    {
+        $response = $this->validXMLCredentialRequest();
+        $this->assertSame((string)(new XMLMissingData()), $response->getContent());
+    }
+
+    public function test_authenticated_standard_requests_with_blank_data_return_400()
+    {
+        $response = $this->withHeaders([
+            'HTTP_AUTHORIZATION' => $this->validCredentials(),
+        ])->postJson(route('kamar'));
+
+        $response->assertJson((new MissingData())->toArray());
+    }
+
+    public function test_authenticated_standard_requests_with_empty_data_return_400()
+    {
+        $response = $this->withHeaders([
+            'HTTP_AUTHORIZATION' => $this->validCredentials(),
+        ])->postJson(route('kamar'), []);
+
+        $response->assertJson((new MissingData())->toArray());
+    }
+
+
+
+
+    public function test_authenticated_standard_xml_requests_with_blank_data_return_400()
+    {
+        $response = $this->withHeaders([
+            'content-type' => 'application/xml',
+            'HTTP_AUTHORIZATION' => $this->validCredentials(),
+        ])->post(route('kamar'));
+
+        $response->assertSee((string) (new XMLMissingData()), false);
+    }
+
+    public function test_authenticated_standard_xml_requests_with_empty_data_return_400()
+    {
+        $response = $this->withHeaders([
+            'content-type' => 'application/xml',
+            'HTTP_AUTHORIZATION' => $this->validCredentials(),
+        ])->post(route('kamar'));
+
+        $response->assertSee((string) (new XMLMissingData()), false);
+    }
+
+    public function test_authenticated_check_requests_return_success()
+    {
+        $response = $this->withHeaders([
+            'HTTP_AUTHORIZATION' => $this->validCredentials(),
+        ])->postJson(
+            route('kamar'),
+            ['SMSDirectoryData' => ['sync' => 'check']]
+        );
+
+        $response->assertJson((new CheckSuccess())->toArray());
+    }
+
+    public function test_authenticated_standard_requests_return_success()
+    {
+        $response = $this->withHeaders([
+            'HTTP_AUTHORIZATION' => $this->validCredentials(),
+        ])->postJson(
+            route('kamar'),
+            ['SMSDirectoryData' => ['sync' => 'part']]
+        );
+
+        $response->assertJson((new Success())->toArray());
+    }
+
+
+
+
+
+    public function test_it_tries_to_store_the_part_file()
+    {
+        Storage::shouldReceive('disk->put')->once()->andReturn(true);
+
+        $this->postJson(
+            '/kamar',
+            ['SMSDirectoryData' => ['sync' => KamarData::SYNC_TYPE_PART]],
+            [
+                'content-type' => 'application/json',
+                'HTTP_AUTHORIZATION' => "Basic " . base64_encode(config('kamar-directory-services.username') . ':' . config('kamar-directory-services.password'))
+            ]
+        );
+    }
+
+    public function test_it_stores_a_part_file()
+    {
+        Storage::fake(config('kamar-directory-services.storageDisk'));
+
+        $this->postJson(
+            '/kamar',
+            ['SMSDirectoryData' => ['sync' => KamarData::SYNC_TYPE_PART]],
+            [
+                'content-type' => 'application/json',
+                'HTTP_AUTHORIZATION' => "Basic " . base64_encode(config('kamar-directory-services.username') . ':' . config('kamar-directory-services.password'))
+            ]
+        );
+
+        $this->assertFalse(
+            empty(Storage::disk(config('kamar-directory-services.storageDisk'))->allFiles(config('kamar-directory-services.storageFolder')))
+        );
+    }
+
+    private function validCredentials()
+    {
+        return "Basic " . base64_encode(config('kamar-directory-services.username') . ':' . config('kamar-directory-services.password'));
+    }
+
+    private function validJsonCredentialRequest()
+    {
+        return $this->postJson('/kamar', [], [
+            'HTTP_AUTHORIZATION' => $this->validCredentials()
+        ]);
+    }
+
+    private function validXMLCredentialRequest()
+    {
+        return $this->post('/kamar', [], [
+            'HTTP_AUTHORIZATION' => $this->validCredentials(),
+            'content-type' => 'application/xml'
+        ]);
+    }
+}
